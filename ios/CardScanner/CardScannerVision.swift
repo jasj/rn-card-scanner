6134828e8fea38fb162d3833954f4ef7582a2a69
Juan Andres Segreda Johanning <andres@segreda.com>
2024-02-12 20:37:25 -0600

    Update CardScannerVision.swift
    arreglo error de duplicacion

--- ios/CardScanner/CardScannerVision.swift
+++ ios/CardScanner/CardScannerVision.swift
@@ -3,7 +3,7 @@
 //  CardScanner
 //
 //  Created by Howl on 04/06/2022.
-//  Copyright © 2022 Facebook. All rights reserved.
+//  © 2022 Facebook. All rights reserved.
 //
 
 import Foundation
@@ -15,56 +15,55 @@ import AVFoundation
 class CardScannerVision : UIView, CameraViewDelegate {
     private var cameraViewMaskLayerColor: UIColor = .black
     private var cameraViewMaskAlpha: CGFloat = 0.6
-    
+
     private lazy var cameraView: CameraView = CameraView(
         delegate: self,
         maskLayerColor: self.cameraViewMaskLayerColor,
         maskLayerAlpha: self.cameraViewMaskAlpha
     )
-    
+
     /// Analyzes text data for credit card info
     private lazy var analyzer = ImageAnalyzer(delegate: self)
-    
+
     @objc var onDidScanCard: RCTDirectEventBlock?
-    
+
     @objc var frameColor: String = "#007aff" {
         didSet {
             print(UIColor(hexString: frameColor))
             frameImageView.tintColor = UIColor(hexString: frameColor)
         }
     }
-    
+
     let frameImageView: UIImageView = {
         let theImageView = UIImageView()
         theImageView.contentMode = .center
         return theImageView
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(cameraView)
         self.addSubview(frameImageView)
     }
-    
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func didMoveToWindow() {
         super.didMoveToWindow()
         let isVisible = (self.superview != nil) && (self.window != nil)
-        if(isVisible) {
-            AVCaptureDevice.authorize { [weak self] authoriazed in
-                guard let strongSelf = self else {
-                    return
-                }
-                guard authoriazed else {
-                    return
-                }
+        if isVisible {
+            AVCaptureDevice.authorize { [weak self] authorized in
+                guard let strongSelf = self else { return }
+                guard authorized else { return }
                 strongSelf.cameraView.setupCamera()
             }
-        }else {
+        } else {
             self.cameraView.stopSession()
         }
-        
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         cameraView.frame = self.bounds
@@ -79,44 +78,38 @@ class CardScannerVision : UIView, CameraViewDelegate {
         let newImageSize = CGSize(width: cuttedWidth, height: cuttedHeight)
         frameImageView.image = portraitFrame?.resizeImageTo(size: newImageSize)?.withRenderingMode(.alwaysTemplate)
     }
-    
-    public func toggleFlash() {
-        self.cameraView.toggleFlash()
-    }
-    
-    public func resetResult() {
-        self.cameraView.stopSession()
-        self.cameraView.startSession()
-    }
-    
-    public func stopCamera() {
-        self.cameraView.stopSession()
-    }
-    
-    public func startCamera() {
-        if(self.cameraView.isRunning()){
-            return;
-        }
-        self.cameraView.startSession()
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError( "init(coder:) has not been implemented" )
-    }
-    
-}
 
-@available(iOS 13, *)
-extension CardScannerVision: CameraViewDelegate {
+    // MARK: - CameraViewDelegate Methods
     internal func didCapture(image: CGImage) {
         analyzer.analyze(image: image)
     }
-    
+
     internal func didError(with error: CreditCardScannerError) {
         DispatchQueue.main.async { [weak self] in
             guard let strongSelf = self else { return }
             strongSelf.cameraView.stopSession()
         }
+    }
+
+    // MARK: - Control Camera
+    public func toggleFlash() {
+        self.cameraView.toggleFlash()
+    }
+
+    public func resetResult() {
+        self.cameraView.stopSession()
+        self.cameraView.startSession()
+    }
+
+    public func stopCamera() {
+        self.cameraView.stopSession()
+    }
+
+    public func startCamera() {
+        if self.cameraView.isRunning() {
+            return
+        }
+        self.cameraView.startSession()
     }
 }
 
@@ -128,24 +121,24 @@ extension CardScannerVision: ImageAnalyzerProtocol {
             DispatchQueue.main.async { [weak self] in
                 guard let strongSelf = self else { return }
                 strongSelf.cameraView.stopSession()
-                if((strongSelf.onDidScanCard) != nil) {
+                if let onDidScanCard = strongSelf.onDidScanCard {
                     var yearStr = ""
                     var monthStr = ""
-                    if(creditCard.expireDate != nil) {
-                        let date = Calendar.current.date(from: creditCard.expireDate!)
+                    if let expireDate = creditCard.expireDate {
+                        let date = Calendar.current.date(from: expireDate)!
                         let formatter = DateFormatter()
                         formatter.dateFormat = "MM"
-                        monthStr = formatter.string(from: date!)
+                        monthStr = formatter.string(from: date)
                         formatter.dateFormat = "yy"
-                        yearStr = formatter.string(from: date!)
+                        yearStr = formatter.string(from: date)
                     }
                     let cardData: [AnyHashable: Any] = [
                         "cardNumber": creditCard.number ?? "",
-                        "expiryMonth":monthStr,
+                        "expiryMonth": monthStr,
                         "expiryYear": yearStr,
                         "holderName": creditCard.name ?? ""
                     ]
-                    strongSelf.onDidScanCard!(cardData)
+                    onDidScanCard(cardData)
                 }
             }
             
@@ -158,8 +151,8 @@ extension CardScannerVision: ImageAnalyzerProtocol {
     }
 }
 
-@available(iOS 13, *)
 extension AVCaptureDevice {
+    @available(iOS 13, *)
     static func authorize(authorizedHandler: @escaping ((Bool) -> Void)) {
         let mainThreadHandler: ((Bool) -> Void) = { isAuthorized in
             DispatchQueue.main.async {
@@ -186,6 +179,6 @@ extension UIImage {
         self.draw(in: CGRect(origin: CGPoint.zero, size: size))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return resizedImage!
+        return resizedImage
     }
 }
